cfd8ddb312470c2acc0542732c56d8a4cc9fc456
brook-mugiwara <brook.mugiwara99@gmail.com>
2024-02-13 12:13:54 +0530

    com

--- Project1/LearnHub/views.py
+++ Project1/LearnHub/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("Hi Drishtti, How are you ???")
+    return HttpResponse("Meow Meow Nigga !!")
